3719d80f14c508426970388b2887e198b5afb24a
Pawel Grzybek <studiorgb@users.noreply.github.com>
2015-08-14 06:17:43 +0100

    jscs enable

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,24 @@
-var gulp = require('gulp'),
-  browserSync = require('browser-sync'),
-  sass = require('gulp-sass'),
-  autoprefixer = require('gulp-autoprefixer'),
-  parker = require('gulp-parker'),
-  uglify = require('gulp-uglify'),
-  messages = {
-    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
-  };
-
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var parker = require('gulp-parker');
+var uglify = require('gulp-uglify');
+var messages = {
+  jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
 
 // Build jekyll project
-gulp.task('jekyll-build', function (done) {
+gulp.task('jekyll-build', function(done) {
   browserSync.notify(messages.jekyllBuild);
   require('child_process').spawn('jekyll', ['build', '--drafts'], {stdio: 'inherit'})
     .on('close', done);
 });
 
-
 // Rebuild and refresh jekyll project
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+gulp.task('jekyll-rebuild', ['jekyll-build'], function() {
   browserSync.reload();
 });
-
 
 // Start BrowserSync Server and serve _site directory
 gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
@@ -32,9 +29,8 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
   });
 });
 
-
 // Compile sass, minify css, autoprefix
-gulp.task('sass', function () {
+gulp.task('sass', function() {
   gulp.src('_sass/main.scss')
     .pipe(sass({
       outputStyle: 'compressed',
@@ -47,22 +43,19 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('_includes'));
 });
 
-
 // Minify js files
-gulp.task('uglify', function () {
+gulp.task('uglify', function() {
   gulp.src('_js/scripts.js')
     .pipe(uglify())
     .pipe(gulp.dest('_includes'));
 });
 
-
 // Watch sass and all html posts
-gulp.task('watch', function () {
+gulp.task('watch', function() {
   gulp.watch('_sass/**/*.scss', ['sass', 'jekyll-rebuild']);
   gulp.watch('_js/*.js', ['uglify', 'jekyll-rebuild']);
   gulp.watch(['index.html', '_layouts/*.html', '_includes/*.html', '_posts/*', '_drafts/*'], ['jekyll-rebuild']);
 });
-
 
 // CSS Analysis
 gulp.task('parker', function() {
